4bde9cb460e5ba650d150d968b76d20f5af8a7e0
PK-1304 <pushkark1304@gmail.com>
2026-06-12 16:49:05 +0530

    problem_445

---
+++ Linked List/445_Add_two_numbers_II.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode *reverseLL(ListNode *head){
+        ListNode *curr = head, *prev = NULL, *next = NULL;
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseLL(l1);
+        l2 = reverseLL(l2);
+        int carry = 0;
+        ListNode *res = new ListNode(0);
+        ListNode *temp2 = res;
+        while(l1 || l2 || carry){
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum/10;
+            ListNode *temp = new ListNode(sum%10);
+            temp2->next = temp;
+            temp2 = temp;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        res = reverseLL(res->next);
+        return res;
+    }
+};
